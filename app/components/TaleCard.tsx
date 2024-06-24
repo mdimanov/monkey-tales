722fb95b801d2895a0ad8ba667b2458492e25673
Miroslav Dimanov <miroslav.dimanov86@gmail.com>
2024-06-24 10:46:22 +0300

    Rremove truncate

--- app/components/TaleCard.tsx
+++ app/components/TaleCard.tsx
@@ -24,9 +24,7 @@ const TaleCard = ({ title, description, imgUrl, taleId }: TaleCardProps) => {
           className="aspect-square h-fit w-full rounded-xl transition-all duration-500 shadow hover:shadow-purple"
         />
         <div className="flex flex-col">
-          <h2 className="text-16 truncate pb-2 text-white-1 font-bold">
-            {title}
-          </h2>
+          <h2 className="text-16 pb-2 text-white-1 font-bold">{title}</h2>
           <p className="text-12 text-white-500">{description}</p>
         </div>
       </figure>
