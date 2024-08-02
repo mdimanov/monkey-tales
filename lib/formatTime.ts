63e693c8491d3c7c55e1b3604cab5419cd5c9c8c
Miroslav Dimanov <miroslav.dimanov86@gmail.com>
2024-08-02 15:52:22 +0300

    Time functions prettier format

--- lib/formatTime.ts
+++ lib/formatTime.ts
@@ -4,8 +4,8 @@ export const formatTime = (seconds: number) => {
     return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
   };
 
-  // Helper function to format time
-  export const formatDuration = (milliseconds: number): string => {
+// Helper function to format time
+export const formatDuration = (milliseconds: number): string => {
   const totalSeconds = Math.floor(milliseconds / 1000);
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
